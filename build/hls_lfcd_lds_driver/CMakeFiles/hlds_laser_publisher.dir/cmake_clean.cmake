5407076e627c37917ae49cc45b9aa2fc2f7e85f9
Class Account <ee106a-aaz@c105-1.local>
2022-12-04 13:26:14 -0800

    wayne

---
+++ build/hls_lfcd_lds_driver/CMakeFiles/hlds_laser_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/cc/ee106a/fa22/class/ee106a-aaz/ros_workspaces/106a-planty-bot/devel/lib/hls_lfcd_lds_driver/hlds_laser_publisher"
+  "/home/cc/ee106a/fa22/class/ee106a-aaz/ros_workspaces/106a-planty-bot/devel/lib/hls_lfcd_lds_driver/hlds_laser_publisher.pdb"
+  "CMakeFiles/hlds_laser_publisher.dir/src/hlds_laser_publisher.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hlds_laser_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/rplidar_ros/CMakeFiles/rplidarNodeClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/cc/ee106a/fa22/class/ee106a-aaz/ros_workspaces/106a-planty-bot/devel/lib/rplidar_ros/rplidarNodeClient"
+  "/home/cc/ee106a/fa22/class/ee106a-aaz/ros_workspaces/106a-planty-bot/devel/lib/rplidar_ros/rplidarNodeClient.pdb"
+  "CMakeFiles/rplidarNodeClient.dir/src/client.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rplidarNodeClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
